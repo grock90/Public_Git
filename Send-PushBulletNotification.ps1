629b0ed31495d0ee8cdbc37b5515cdcffdce05d2
gbarnes <grock90@Hotmail.com>
2016-01-26 17:04:44 -0800

    Added additional parameters
    Removed inline items for Access_Token and EmailAddress and made them
    parameters.  Update the new parameters $AccessToken & $EmailAddress with
    valid pushbullet token and the corresponding email address.

--- Send-PushBulletNotification.ps1
+++ Send-PushBulletNotification.ps1
@@ -1,13 +1,20 @@
 ﻿Param(
+    
     [Parameter(Mandatory=$true)]
     [string] $Message,
 
     [Parameter(Mandatory=$true)]
-    [string] $Title
+    [string] $Title,
+    
+    [Parameter(Mandatory=$false)]
+    [string] $AccessToken = "YOUR_PUSHBULLET_TOKEN",
+    
+    [Parameter(Mandatory=$false)]
+    [string] $EmailAddress = "YOUR EMAIL ADDRESS USED BY PUSHBULLERT"   
 )
 
 $Header = @{}
-$Header.Add('Access-Token','<YOUR_PUSHBULLET_TOKEN>')
+$Header.Add('Access-Token',"$AccessToken")
 $Header.Add('Content-Type','application/json')
 
 $Body = [PSCustomObject]@{  
@@ -18,7 +25,7 @@ $Body = [PSCustomObject]@{
 $Payload = $Body | ConvertTo-Json
 
 $PushBulletMe = Invoke-RestMethod -Method Get -Uri "https://api.pushbullet.com/v2/users/me" -Headers $Header
-if($PushBulletMe.email -eq '<YOUR_EMAIL_ADDRESS>'){
+if($PushBulletMe.email -eq "$EmailAddress"){
     Write-Host "Session is Active: Sending New Message"
     $PushNotification = Invoke-RestMethod -Method Post -Uri "https://api.pushbullet.com/v2/pushes" -Headers $Header -Body $Payload
     if($PushNotification.dismissed -ne $true){
