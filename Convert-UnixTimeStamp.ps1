31b0a44b30235c1675278f4b1eda24d3faec566d
gbarnes <grock90@Hotmail.com>
2016-02-01 22:34:40 -0800

    Dealing with Unix time stamps
    The RabbitMQ made me do it! This doesn't elude me to when something
    occurred "timestamp:1454393518".   Now it's a non issue.  Returns date
    as an object for further use, or just use the code inside the function
    to your hearts content.

---
+++ Convert-UnixTimeStamp.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.Synopsis
+   Converts a timestamp into a datetime value 
+.DESCRIPTION
+   When given a valid timestamp this tool will convert it
+   to a human readable date time string. 
+.EXAMPLE
+   Convert-TimeStampToDateTime -Timestamp 1445031547
+#>
+
+[CmdletBinding()]
+[OutputType([int])]
+Param
+(
+    # Param1 help description
+    [Parameter(Mandatory=$true,
+                ValueFromPipelineByPropertyName=$true,
+                Position=0)]
+    [int]$Timestamp
+)
+
+function Convert-UnixDate($UnixTimeStamp){ 
+    [datetime]$EpochTime = "1970-01-01 00:00:00"
+    [timezone]::CurrentTimeZone.ToLocalTime($EpochTime).AddSeconds($UnixTimeStamp)
+}
+
+$ConvertedUnixTime = Convert-UnixDate -UnixTimeStamp $Timestamp 
+
+Write-Host -BackgroundColor Green -ForegroundColor Black "Converted Unix Time: "   -Verbose
+$ConvertedUnixTime
+ 
+ 
