e86870273eff5f24f51a279b2b177eaa4b89705f
gbarnes <grock90@Hotmail.com>
2016-02-29 09:24:45 -0800

    Decode Saml2 Packets
    Given an encoded SAML2 packet, decode the packet to determine the
    SignatureMethod and EcryptionMethod

---
+++ Invoke-DecodeSaml2Packet.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.Synopsis
+   Decode a Saml2 encrypted packet to determine certificate value
+.DESCRIPTION
+   Given a Saml2 encoded packet, decode packet and expose the Signature and EncryptionMethod
+.EXAMPLE
+   Invoke-DecodeSame2Packet.ps1 -SamlResponse "SamlResponsePacketHere" -HttpCertSubject 'myTestCert'
+#>
+
+Param(
+    [Parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=0)]
+    [ValidateNotNull]
+    $SAMLResponse, 
+    
+    [Parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=1)]
+    [ValidateNotNull]
+    $HttpCertSubject
+    
+)
+
+function Get-Base64DecodedResponse{
+    
+    [xml]$DecodedResponse = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($SAMLResponse))
+     $DecodedResponse.Response
+     Write-Host "Status: "  
+     $DecodedResponse.Response.Status.StatusCode.Value
+     Write-Host "`nSignatureMethod"
+     $DecodedResponse.Response.Signature.SignedInfo.SignatureMethod
+     Write-Host "`nEncryptionMethod"
+     $DecodedResponse.Response.EncryptedAssertion.EncryptedData.EncryptionMethod
+}
